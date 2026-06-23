17930ee46a38f78faac1f8c1c903da756bc5baab
Kourosh-Delavar <delavar.kourosh@gmail.com>
2026-06-23 16:30:29 +0200

    feat: add backward check

--- include/modelchecklib/algorithms/Core/Reachability/InvariantChecking.h
+++ include/modelchecklib/algorithms/Core/Reachability/InvariantChecking.h
@@ -49,39 +49,68 @@ namespace modelchecklib::algorithms {
             return true; // no violation found, continue the search 
         };
 
-        breadthFirstSearch(graph, graph.getInitialStates(), visitor);
+        auto forward_navigator = [](const Graph& g, StateId current, auto yield) {
+            for (const auto& trans : g.getOutgoingTransitions(current)) {
+                yield(trans.to); 
+            }
+        };
+
+        breadthFirstSearch(graph, graph.getInitialStates(), visitor, forward_navigator);
 
         return result;
     }
 
-    // template<typename Graph>
-    // InvariantResult<Graph> checkInvariantBackward(const Graph&, typename Graph::APId bad_ap) {
-    //     using StateId = typename Graph.StateId;
+    template<typename Graph>
+    InvariantResult<Graph> checkInvariantBackward(const Graph&, typename Graph::APId bad_ap) {
+        using StateId = typename Graph.StateId;
 
-    //     InvariantResult<Graph> result{ // default as holds (true)
-    //         true,
-    //         {}
-    //     };
+        InvariantResult<Graph> result{ // default as holds (true)
+            true,
+            {}
+        };
 
-    //     if (graph.stateCount() == 0) return result;
+        if (graph.stateCount() == 0) return result;
 
-    //     // find all the states that violate the invariant
-    //     std::vector<StateId> bad_states;
-    //     for (StateId i = 0; i < graph.stateCount(); ++i) {
-    //         if (graph.getState(i).hasAP(bad_ap)) {
-    //             bad_states.push_back(i);
-    //         }
-    //     }
+        // find all the states that violate the invariant
+        std::vector<StateId> bad_states;
+        for (StateId i = 0; i < graph.stateCount(); ++i) {
+            if (graph.getState(i).hasAP(bad_ap)) {
+                bad_states.push_back(i);
+            }
+        }
 
-    //     if (bad_states.empty()) return result; // if no bad states found, return default result
+        if (bad_states.empty()) return result; // if no bad states found, return default result
 
-    //     std::vector<bool> visisted(graph.stateCount(), false);
-    //     std::vector<StateId> parent_map(graph.stateCount(), Graph::INVALID_STATE);
+        std::vector<StateId> parent_map(graph.stateCount(), Graph::INVALID_STATE);
 
-    //     std::vector<StateId> queue;
-    //     std::size_t head = 0;
+        auto visitor = [&](StateId current, StateId predecessor) -> bool {
+            if (parent_map[current] == Graph::INVALID_STATE) {
+                parent_map[current] = predecessor;
+            }
 
+            if (graph.getState(current).isInitial()) {
+                result.holds = false;
+                
+                StateId trace_node = current;
+                while (trace_node != parent_map[trace_node]) {
+                    result.counterexample_trace.push_back(trace_node);
+                    trace_node = parent_map[trace_node];
+                }
+                result.counterexample_trace.push_back(trace_node); // add the initial state
+                
+                return false; // found a violation, stop the search
+            }
+            return true; // no violation found, continue the search
+        };
 
-    // }
-    
+        auto backward_navigator = [](const Graph& g, StateId current, auto yield) {
+            for (const auto& trans : g.getIncomingTransitions(current)) {
+                yield(trans.from); 
+            }
+        };
+
+        breadthFirstSearch(graph, bad_states, visitor, backward_navigator);
+
+        return result;
+    }
 }
